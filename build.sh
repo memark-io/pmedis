2533c8dd2ea14ad3b299135edbd216d12d5afae8
Yang Jun01 <yangjun01@4paradigm.com>
2022-03-02 16:45:22 +0800

    update CI

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git submodule update --init --recursive
 pushd kvdk
 rm -rf build && mkdir -p build && pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCHECK_CPP_STYLE=ON -DBUILD_TESTING=OFF && make -j`nproc` && popd
@@ -7,4 +6,3 @@ popd
 rm -rf build && mkdir -p build && pushd build
 cmake .. && make -j`nproc` && popd
 cp kvdk/build/libengine.so test/ && cp build/lib/libpmedis.so test/
-
