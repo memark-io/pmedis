024117e68d182106ac02e0b88690f566fa307640
chencheng <chencheng@node-4.sg.4pd.io>
2022-03-15 11:10:48 +0800

    code check util functions

--- src/util.c
+++ src/util.c
@@ -1,4 +1,5 @@
 #include "util.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/util.h
+++ src/util.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-
 int string2ll(const char *s, size_t slen, long long *value);
 char *safeStrcat(char *__restrict s1, size_t s1_size, const char *__restrict s2,
                  size_t s2_size);
