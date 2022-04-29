426b6e3811223cc08dcc4edd46506bb331d82d76
Yang Jun01 <yangjun01@4paradigm.com>
2022-04-29 15:06:06 +0800

    update cmake

--- build_debug.sh
+++ build_debug.sh
@@ -4,5 +4,5 @@ rm -rf build && mkdir -p build && pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCHECK_CPP_STYLE=OFF -DBUILD_TESTING=OFF && make -j`nproc` && popd
 popd
 rm -rf build && mkdir -p build && pushd build
-cmake -DCMAKE_BUILD_TYPE=Release .. && make -j`nproc` && popd
+cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j`nproc` && popd
 rm -rf test/*.so && cp kvdk/build/*.so test/ && cp build/lib/*.so test/
